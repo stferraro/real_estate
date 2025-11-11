fe1d64d528ca60285f5250cd502478cf4935e827
stferraro <gerferr83@gmail.com>
2025-11-11 18:00:20 -0400

    first changing

---
+++ __manifest__.py
@@ -0,0 +1,27 @@
+{
+    'name': 'Real Estate in Odoo',
+    'version': '18.0.1.0.0',
+    'summary': 'A Module For Real Estate administration',
+    'author': 'Gerardo Alí Ferraro Schelijasch',
+    'license': 'LGPL-3',
+    'category': 'Other',
+    'images': ['static/description/icon.png'],
+    'data': [
+        #security
+        'security/ir.model.access.csv',
+
+        # views and inherit views
+        'views/estate_property_views.xml',
+        'views/estate_property_type_views.xml',
+        'views/estate_property_tags_views.xml',
+        'views/estate_property_offer_views.xml',
+        'views/res_users_views.xml',
+
+        #menus
+        'data/real_estate_menus.xml',
+    ],
+    'depends': [
+        'base',
+    ],
+    'application': True,
+}
---
+++ models/estate_property_type.py
@@ -0,0 +1,56 @@
+from odoo import models, fields, _
+
+class EstatePropertyType(models.Model):
+    _name = 'estate.property.type'
+    _description = 'Estate Property Type'
+    _order = 'sequence'
+
+    sequence = fields.Integer(
+        default=1,
+        help="Sequence for ordering property types"
+    )
+
+    name = fields.Char(
+        required=True
+    )
+
+    property_ids = fields.One2many(
+        'estate.property',
+        'property_type_id',
+        string='Properties',
+        help='Properties of this type'
+    )
+
+    offer_ids = fields.One2many(
+        'estate.property.offer',
+        'property_type_id',
+        string='Offers',
+        help='Offers for properties of this type'
+    )
+
+    offer_count = fields.Integer(
+        compute='_compute_offer_count',
+        string='Offer Count',
+        help='Number of offers for properties of this type'
+    )
+
+    _sql_constraints = [
+        ('name_unique', 'unique(name)', 'The property type name must be unique.')
+    ]
+
+    def _compute_offer_count(self):
+        for rec in self:
+            rec.offer_count = len(rec.offer_ids)
+
+    def action_open_offer_ids(self):
+        self.ensure_one()
+        return {
+            'name': _('Offers',),
+            'type': 'ir.actions.act_window',
+            'view_mode': 'list,form',
+            'res_model': 'estate.property.offer',
+            'domain': [('property_type_id', '=', self.id)],
+            'context': {'default_property_type_id': self.id},
+        }
+    
+    
